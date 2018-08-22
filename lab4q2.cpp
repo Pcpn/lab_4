faf25a1fe94e6ec73a93a94c6ab16a8ac67afc4d
Pcpn <42600610+Pcpn@users.noreply.github.com>
2018-08-22 15:08:16 +0530

    Add files via upload

---
+++ lab4q2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+// declare variables
+float celsiusC,fahrenheitF;
+//enter temperature in C
+cout << "What is the temperature in C? ";
+cin >> celsiusC;
+// convert into fahrenheit
+celsiusC = (fahrenheitF-32)/9*5;
+// display the output .
+cout<<" The temperature in F" << celsiusC<<endl;
+}
