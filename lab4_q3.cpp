5f3f70665aedffd431bdd7c7bdea2db3a5ad2f0d
Pcpn <42600610+Pcpn@users.noreply.github.com>
2018-08-22 15:50:46 +0530

    Add files via upload

---
+++ lab4_q3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//declare variables
+float C, F;
+//Temperature in F
+cout << "What is the temperature in F?";
+cin >> F;
+//convert into celsius 
+C = 1.8*F+32;
+// display the output .
+cout<<"The temperature in C is " <<C<<endl;
+} 
+
