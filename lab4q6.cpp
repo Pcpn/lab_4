d402c229a01754430851d2c8696a3af9b2577a08
Pcpn <42600610+Pcpn@users.noreply.github.com>
2018-08-22 16:51:44 +0530

    Add files via upload

---
+++ lab4q6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+//declaring variables
+ float A, H, B;
+//enter the base and height of triangle
+cout << " product of height and base";
+cin>> B >> H;
+//area of the triangle
+A = (B*H)/2; 
+//display output
+cout << " the area of the triangle is " << A << endl;
+}
