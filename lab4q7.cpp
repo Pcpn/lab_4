0131f3df49151340864408aa9ed6266709a15ffd
Pcpn <42600610+Pcpn@users.noreply.github.com>
2018-08-22 17:16:39 +0530

    Add files via upload

---
+++ lab4q7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+//declaring variables
+float a, A;
+//side of the equilateral triangle 
+cout << " the side of equilateral triangle";
+cin >> a;
+//area of equilateral triangle
+A = sqrt(3)/4*(a*a);
+//display output
+cout <<" the area of equilateral triangle is" << A << endl;
+}
