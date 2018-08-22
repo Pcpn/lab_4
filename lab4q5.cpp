384de5926a7ddd2e08a4e247bf8e78c759edc93b
Pcpn <42600610+Pcpn@users.noreply.github.com>
2018-08-22 16:37:16 +0530

    Add files via upload

---
+++ lab4q5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+// declaring variables
+float A1, A2, A3;
+//enter two angles of triangle
+cout << "sum of two angles of triangle";
+cin >> A1 >> A2;
+//find the third angle of triangle
+A3 = 180-(A1+A2);
+//display output
+cout << " the third angle of triangle is" << A3 << endl;
+}
+
